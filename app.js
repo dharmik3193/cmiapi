93b6d64954c89ec6738c66928bd2c9ef0786ed7d
dharmik3193 <115345608+dharmik3193@users.noreply.github.com>
2023-02-03 19:08:38 +0530

    cors update

--- app.js
+++ app.js
@@ -13,9 +13,12 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(cors({
-  origin: 'https://cmi.vercel.app/'
-}));
+app.use(function(req,res,next){
+   res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+               next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
